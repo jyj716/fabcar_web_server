598c23f0755ceb3d9a56ce9492f951f210fb53d7
dong <you@example.com>
2020-07-08 11:19:24 +0900

    index set

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var path = require('path')
 var bodyParser = require('body-parser')
 require('ejs')
 require('dotenv').config()
-
+var apiRouter = require('./routes/Router')
 
 app.set('views', path.resolve(__dirname + '/views'))
 app.set('view engine', 'ejs')
@@ -13,19 +13,18 @@ app.set('view engine', 'ejs')
 
 app.use(bodyParser.urlencoded({extended:true}))
 app.use(bodyParser.json())
-
+app.use('/', apiRouter)
 
 
 //cors 
-app.get('/',(req,res,next)=>{
-res.send("success")
-})
 
 
 
 var cors = require('cors')
 app.use(cors())
 var port =process.env.PORT || 3000
+
+
 app.listen(port, ()=>{
   console.log(`Server is Starting at http://localhost:${port}`)
 })
